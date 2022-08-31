0f020f8dc20b49affe5793bb0a96bbe804cfddc3
Bruno Silva Dos Santos <Brunorjcf@gmail.com>
2022-08-31 00:20:14 -0300

    primeiro commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+function HomePage() {
+    return (
+        <div>
+            <h2>bem-vindo ao meu website!</h2>
+             Navegar para <Link href="/sobre">
+                                <a>Sobre</a>
+                          </Link>
+        </div>
+    );
+     
+  }
+  
+  export default HomePage
---
+++ pages/sobre.js
@@ -0,0 +1,5 @@
+function Sobre() {
+    return <div>Bem-vindo a página sobre!</div>
+  }
+  
+  export default Sobre
